1d3ebba9b58a5d7811c87fec98ccfe173273d841
Thalita Policarpo <thalita_policarpo@outlook.com>
2024-06-06 12:39:45 -0300

    Initial commit

---
+++ src/main/java/com/example/ensinotp03springboot/EnsinoTp03SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ensinotp03springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsinoTp03SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsinoTp03SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ensinotp03springboot/EnsinoTp03SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ensinotp03springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsinoTp03SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
